d0a8ca889049d66fd04901269b2a9b3596eb19ac
Paul Cruz <paulcruz74@gmail.com>
2020-05-15 18:11:05 -0500

    completed solution for 141A

--- 141A/solution/solution.cpp
+++ 141A/solution/solution.cpp
@@ -6,5 +6,30 @@ int main() {
   unordered_map<char, int> umap;
   string s1, s2, s3;
   cin >> s1 >> s2 >> s3;
-  cout << s1;
+  for (int i = 0; i < s1.length(); i++) {
+    umap[s1[i]]++;    
+  }
+  
+  for (int i = 0; i < s2.length(); i++) {
+    umap[s2[i]]++;    
+  }
+  
+  for (int i = 0; i < s3.length(); i++) {
+    if (umap[s3[i]] <= 0) {
+      cout << "NO" << endl;
+      return 0;
+    }
+
+    umap[s3[i]]--;
+  }
+
+  unordered_map<char, int>:: iterator itr; 
+  for (itr = umap.begin(); itr != umap.end(); itr++) {
+    if (itr->second) {
+      cout << "NO" << endl;
+      return 0;
+    }
+  }
+
+  cout << "YES" << endl;
 }
