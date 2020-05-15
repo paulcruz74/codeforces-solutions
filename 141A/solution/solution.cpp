a0b98f55a3314838db0de3ab82108e6588fd05c2
Paul Cruz <paulcruz74@gmail.com>
2020-05-15 17:52:44 -0500

    adding gitignore as well as solution base

---
+++ 141A/solution/solution.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+int main() {
+  unordered_map<char, int> umap;
+  string s1, s2, s3;
+  cin >> s1 >> s2 >> s3;
+  cout << s1;
+}
