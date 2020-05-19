656190e81513699698e24ff964e219688f696d10
Paul Cruz <paulcruz74@gmail.com>
2020-05-18 22:42:40 -0500

    adding solution for 460A

---
+++ 460A/solution/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  int days = 0;
+  while (n > 0) {
+    days++;
+    n--;
+    if (days % m == 0) n++;
+  }
+
+  cout << days << endl;
+}
